39c0a80b5a6b37d17bf96e65df6b0aea38523be2
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:19 +0100

    converting to typescript file:  app/controller/services/getContactIDs.ts

---
+++ app/controller/services/getContactIDs.ts
@@ -0,0 +1,35 @@
+const eppSession = require('../../client/session/session')
+const eppContact = require("../../client/object/contact/contact")
+import { Contact } from "../../Models/models"
+
+const getContacts = (contacts:Contact[]) => {
+    return new Promise((resolve, reject) => {
+        
+        let promisses:Promise<any>[] = []
+        eppSession.hello().then(() => {
+            console.log("Connected to epp server: ")
+            eppSession.login().then(() => {
+                contacts.forEach((contact) => {
+                    promisses.push(eppContact.getInfoByEmail(contact))
+                })
+                Promise.all(promisses).then((res) => {
+                    eppSession.logout().then(() => {
+                        resolve(res)
+                    }).catch((err:any) => {
+                        reject(err)
+                    })
+                }).catch((err) => {
+                    reject(err)
+                })
+            }).catch((err:any) => {
+                reject(err)
+            })
+        }).catch((err:any) => {
+            reject(err)
+        })
+    })
+}
+
+module.exports = {
+    getContacts
+}
