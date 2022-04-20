405f5576febfc81a222fe2814210f4732a51f34b
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:18 +0100

    converting to typescript file:  app/client/session/session.ts

---
+++ app/client/session/session.ts
@@ -0,0 +1,15 @@
+const Messages = require('./messages')
+const transporter = require('../../utils/transporter')
+const Session = {
+    login: () => {
+        return transporter.send(Messages.login())
+    },
+    logout: () => {
+        return transporter.send(Messages.logout())
+    },
+    hello: () => {
+        return transporter.send(Messages.hello())
+    }
+}
+
+module.exports = Session
