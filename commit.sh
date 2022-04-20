358916a9c6ba93e62deb5be8f02663351fae306d
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:36 +0100

    converting to typescript file:  commit.sh

---
+++ commit.sh
@@ -0,0 +1,89 @@
+git add app/client/object/contact/messages.ts
+git commit -m "converting to typescript file:  app/client/object/contact/messages.ts"
+git add app/client/object/domain/domain.ts
+git commit -m "converting to typescript file:  app/client/object/domain/domain.ts"
+git add app/client/object/domain/messages.ts
+git commit -m "converting to typescript file:  app/client/object/domain/messages.ts"
+git add app/client/object/host/host.ts
+git commit -m "converting to typescript file:  app/client/object/host/host.ts"
+git add app/client/object/host/messages.ts
+git commit -m "converting to typescript file:  app/client/object/host/messages.ts"
+git add app/client/session/messages.ts
+git commit -m "converting to typescript file:  app/client/session/messages.ts"
+git add app/client/session/session.ts
+git commit -m "converting to typescript file:  app/client/session/session.ts"
+git add app/controller/controller.ts
+git commit -m "converting to typescript file:  app/controller/controller.ts"
+git add app/controller/services/getContactIDs.ts
+git commit -m "converting to typescript file:  app/controller/services/getContactIDs.ts"
+git add app/controller/services/registerContact.ts
+git commit -m "converting to typescript file:  app/controller/services/registerContact.ts"
+git add app/controller/services/registerDomain.ts
+git commit -m "converting to typescript file:  app/controller/services/registerDomain.ts"
+git add app/controller/services/registerHost.ts
+git commit -m "converting to typescript file:  app/controller/services/registerHost.ts"
+git add app/controller/services/whois.ts
+git commit -m "converting to typescript file:  app/controller/services/whois.ts"
+git add app/exceptions/unKnownOperatorException.ts
+git commit -m "converting to typescript file:  app/exceptions/unKnownOperatorException.ts"
+git add app/mail/event.ts
+git commit -m "converting to typescript file:  app/mail/event.ts"
+git add app/mail/generator.ts
+git commit -m "converting to typescript file:  app/mail/generator.ts"
+git add app/mail/mailer.ts
+git commit -m "converting to typescript file:  app/mail/mailer.ts"
+git add app/mail/transporter.ts
+git commit -m "converting to typescript file:  app/mail/transporter.ts"
+git add app/payment/Momo/API/app.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/app.ts"
+git add app/payment/Momo/API/authorisationToken.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/authorisationToken.ts"
+git add app/payment/Momo/API/constants.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/constants.ts"
+git add app/payment/Momo/API/referenceId.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/referenceId.ts"
+git add app/payment/Momo/API/requestToPay.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/requestToPay.ts"
+git add app/payment/Momo/API/user.ts
+git commit -m "converting to typescript file:  app/payment/Momo/API/user.ts"
+git add app/payment/Momo/main.ts
+git commit -m "converting to typescript file:  app/payment/Momo/main.ts"
+git add app/payment/OM/main.ts
+git commit -m "converting to typescript file:  app/payment/OM/main.ts"
+git add app/payment/payer.ts
+git commit -m "converting to typescript file:  app/payment/payer.ts"
+git add app/utils/generator.ts
+git commit -m "converting to typescript file:  app/utils/generator.ts"
+git add app/utils/json.ts
+git commit -m "converting to typescript file:  app/utils/json.ts"
+git add app/utils/logger.ts
+git commit -m "converting to typescript file:  app/utils/logger.ts"
+git add app/utils/settings.ts
+git commit -m "converting to typescript file:  app/utils/settings.ts"
+git add app/utils/transporter.ts
+git commit -m "converting to typescript file:  app/utils/transporter.ts"
+git add app/utils/xml.ts
+git commit -m "converting to typescript file:  app/utils/xml.ts"
+git add app/validator/checkDomain.ts
+git commit -m "converting to typescript file:  app/validator/checkDomain.ts"
+git add app/validator/checkEmail.ts
+git commit -m "converting to typescript file:  app/validator/checkEmail.ts"
+git add app/validator/checkIpAddresse.ts
+git commit -m "converting to typescript file:  app/validator/checkIpAddresse.ts"
+git add app/validator/checkTelephoneNumber.ts
+git commit -m "converting to typescript file:  app/validator/checkTelephoneNumber.ts"
+git add app/validator/index.ts
+git commit -m "converting to typescript file:  app/validator/index.ts"
+git add commit.sh
+git commit -m "converting to typescript file:  commit.sh"
+git add index.ts
+git commit -m "converting to typescript file:  index.ts"
+git add spec/app/mail/transporter.spec.ts
+git commit -m "converting to typescript file:  spec/app/mail/transporter.spec.ts"
+git add spec/app/validator/checkDomain.spec.ts
+git commit -m "converting to typescript file:  spec/app/validator/checkDomain.spec.ts"
+git add spec/app/validator/checkEmail.spec.ts
+git commit -m "converting to typescript file:  spec/app/validator/checkEmail.spec.ts"
+git add spec/app/validator/checkIpAddresse.spec.ts
+git commit -m "converting to typescript file:  spec/app/validator/checkIpAddresse.spec.ts"
+git add spec/app/validator/checkTelephoneNumber.spec.tsgit commit -m "converting to typescript file:  spec/app/validator/checkTelephoneNumber.spec.ts
