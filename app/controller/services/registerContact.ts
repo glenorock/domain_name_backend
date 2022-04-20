305055657e158620a6ebb72753f6f8cb9d0d39c2
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:19 +0100

    converting to typescript file:  app/controller/services/registerContact.ts

---
+++ app/controller/services/registerContact.ts
@@ -0,0 +1,56 @@
+const mailer = require('../../mail/mailer')
+const eppSession = require('../../client/session/session')
+const eppContact = require("../../client/object/contact/contact")
+import { Contact } from "../../Models/models"
+
+const createContactIfNotExists = (contact: Contact) => {
+    return new Promise((resolve, reject) => {
+
+        eppSession.hello().then(() => {
+            console.log("Connected to epp server: ")
+            eppSession.login().then(() => {
+                eppContact.checkByEmail(contact.email).then((res: any) => {
+                    if (res.exists) {
+                        eppContact.create(contact).then((res: any) => {
+                            eppSession.logout().then(() => {
+                                mailer.sendMail("create:contact", contact.email)
+                                resolve(res)
+                            }).catch((err: any) => {
+                                reject(err)
+                            })
+                        }).catch((err: any) => {
+                            reject(err)
+                        })
+                    } else {
+                        resolve(res)
+                    }
+                }).catch((err: any) => {
+                    reject(err)
+                })
+            }).catch((err: any) => {
+                reject(err)
+            })
+        }).catch((err: any) => {
+            reject(err)
+        })
+    })
+}
+
+const createContacts = (contacts: Contact[]) => {
+    return new Promise<any>((resolve, reject) => {
+        let promisses: Promise<any>[] = []
+        contacts.forEach((contact) => {
+            promisses.push(createContactIfNotExists(contact))
+        })
+        Promise.all(promisses).then(
+            (res) => {
+                resolve(res)
+            }).catch((err) => {
+                reject(err)
+            })
+    })
+}
+
+module.exports = {
+    createContacts
+}
