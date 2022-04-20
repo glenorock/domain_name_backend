a1124b84d9a7d8024ada334daf7570da688957af
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:32 +0100

    converting to typescript file:  app/utils/transporter.ts

---
+++ app/utils/transporter.ts
@@ -0,0 +1,29 @@
+import { Logger } from "./logger"
+const config = require('config')
+const Net = require('net')
+
+const host = config.get("cocca.host")
+const port = config.get("cocca.port")
+
+const send = (message:string) => {
+    const client = new Net.Socket()
+    return new Promise((resolve) => {
+        client.connect({
+            port: port,
+            host: host
+        }, () => {
+            console.log('TCP connection established with the server.');
+            client.write(message)
+            client.on('data', function (chunk:any) {
+                let result = chunk.toString()
+                client.end()
+                resolve(result)
+            });
+        })
+
+    })
+}
+
+module.exports = {
+    send
+}
