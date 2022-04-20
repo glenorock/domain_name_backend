1222a608c48040c398aaee3a976bee216b45b8d0
glenorock <glen19.og@gmail.com>
2022-04-20 08:54:24 +0100

    transforming to ts:app/client/object/contact/contact.ts

---
+++ app/client/object/contact/contact.ts
@@ -0,0 +1,26 @@
+const Messages = require('./messages')
+const transporter = require('../../../utils/transporter')
+import { Contact } from "../../../Models/models"
+
+const contact = {
+    checkById: (id:String) => {
+        return transporter.send(Messages.checkById(id))
+    },
+    checkByEmail: (email:String) => {
+        return transporter.send(Messages.checkByEmail(email))
+    },
+    getInfoByEmail: (email:String) => {
+        return transporter.send(Messages.getInfoByEmail(email))
+    },
+    getInfoById: (id:String) => {
+        return transporter.send(Messages.getInfoById(id))
+    },
+    create: (contact:Contact) => {
+        return transporter.send(Messages.create(contact))
+    },
+    update: (contact:Contact) => {
+        return transporter.send(Messages.update(contact))
+    }
+}
+
+module.exports = contact
