4a1a87e672e69c0d60c73c46789917a545e7af00
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:15 +0100

    converting to typescript file:  app/client/object/domain/domain.ts

---
+++ app/client/object/domain/domain.ts
@@ -0,0 +1,14 @@
+import {Domain} from "../../../Models/models"
+const transporter = require('../../../utils/transporter')
+const Messages = require('./messages')
+
+
+const domain = {
+    check: (name:String) => {return transporter.send(Messages.check(name))},
+    info: (name:String) => {return transporter.send(Messages.info(name))},
+    create: (domain:Domain) => {return transporter.send(Messages.create(domain))},
+    renew: (domain: Domain,period:Number) => {return transporter.send(Messages.renew(domain,period))},
+    update: (domain:Domain) => {return transporter.send(Messages.update(domain))},
+}
+
+module.exports = domain
