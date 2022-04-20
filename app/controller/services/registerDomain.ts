185d08facfb5fabbd5ca992e49a8f6335a1473bc
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:20 +0100

    converting to typescript file:  app/controller/services/registerDomain.ts

---
+++ app/controller/services/registerDomain.ts
@@ -0,0 +1,22 @@
+const mailer = require('../../mail/mailer')
+const payer = require('../../payment/payer')
+const eppSession = require('../../client/session/session')
+const eppContact = require("../../client/object/contact/contact")
+const eppHost = require('../../client/object/host/host')
+const eppDomain = require('../../client/object/domain/domain')
+import { Domain } from "../../Models/models"
+const register = (data:Domain,number:string) => {
+    let contacts = [data.contactAdmin,data.contactTech]
+    let hosts = data.hosts
+    return new Promise((resolve, reject) => {
+        payer.pay(number).then(() => {
+            
+        }).catch((err:any) => {
+            reject(err)
+        })
+    })
+}
+
+module.exports = {
+    register
+}
