17cde6135168a145a76d90f491fa667c1fad5c80
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:23 +0100

    converting to typescript file:  app/mail/generator.ts

---
+++ app/mail/generator.ts
@@ -0,0 +1,23 @@
+export const Genrator = {
+    AccountCreation:{
+        subject: "",
+        html: "",
+        parse: () =>{
+
+        }
+    },
+    DomainCreation:{
+        subject: "",
+        html: "",
+        parse: () =>{
+
+        }
+    },
+    DomainUpdate:{
+        subject: "",
+        html: "",
+        parse: () =>{
+
+        }
+    },
+}
