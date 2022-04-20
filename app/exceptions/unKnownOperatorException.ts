1ae9875c1ca167337694066dc7cdbf9bcd07ccf1
glenorock <glen19.og@gmail.com>
2022-04-20 09:28:22 +0100

    converting to typescript file:  app/exceptions/unKnownOperatorException.ts

---
+++ app/exceptions/unKnownOperatorException.ts
@@ -0,0 +1,8 @@
+const UnknownOperatorException = () => {
+    const error = Error("The mobile operator is not recongnised by the system")
+    return error
+}
+
+UnknownOperatorException.prototype = Object.create(Error.prototype)
+
+module.exports = UnknownOperatorException
