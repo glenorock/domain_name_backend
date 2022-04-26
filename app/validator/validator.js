9013e2b3b016ea5c3e234034899e933c6b58ed5b
glenorock <glen19.og@gmail.com>
2022-04-26 10:38:20 +0100

    modifying the format of the validators module

--- app/validator/validator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * @module Validator
- * @param input The input to be validated
- * @description The aim of this module is to validate the different inputs sent into the system
- */
-Object.defineProperty(exports, "__esModule", { value: true });
--- tests/app.validator.ping.ts
+++ tests/app.validator.ping.ts
@@ -1,4 +1,4 @@
-import  * as validator from '../app/validator/checkIpAddresse'
+import  * as validator from '../app/validator/index'
 
 let ping = validator.IpValidator.prototype.isAlive
 
