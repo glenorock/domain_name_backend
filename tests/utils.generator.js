66ee18140fd979905bfe9c6eb7d3a5942b302b3d
FUASHI LOT-BILL <lotbill.fuashi@gmail.com>
2022-06-02 10:48:26 +0100

    renamming app to src

--- tests/utils.generator.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// import  * as generator from '../app/utils/generator'
-// console.log(generator.generateContactIdentifier("Otang Glen Orock"))
-// console.log(generator.generatePassword())
