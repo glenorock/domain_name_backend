e02eac8a71f1a153b1d2b08523420d9f5f203d1a
orock glen <glen19.og@gmail.com>
2022-08-06 14:51:24 +0100

    adding routes to index.ts

--- index.ts
+++ index.ts
@@ -14,17 +14,13 @@ import db from './src/config/db.config';
 
 const app = express()
 db.sequelize.sync()
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
 app.use(cookieParser());
-
 app.use(cors())
-app.get('/', (req:any, res:any) => {
-let responseText = `Hello World!<br>`
-responseText += `<small>Requested at: ${req.requestTime}</small>`
-res.send(responseText)
-})
+
 app.use('/',PayMentRouter)
 app.use('/', EppRouter)
 app.use('contact/',ContactRouter)
