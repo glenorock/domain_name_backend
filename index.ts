58888badf8326d46e545c3af253bab89df068c2e
orock glen <glen19.og@gmail.com>
2022-08-06 14:49:47 +0100

    adding routes to the index.ts file

--- index.ts
+++ index.ts
@@ -5,6 +5,11 @@ import cors from 'cors';
 
 import PayMentRouter from './src/routes/pay.route';
 import EppRouter from './src/routes/epp.route';
+import ContactRouter from './src/routes/contact.route';
+import DomainRouter from './src/routes/domain.route';
+import HostRouter from'./src/routes/host.route';
+import TransactionRouter from './src/routes/transaction.route';
+
 import db from './src/config/db.config';
 
 const app = express()
@@ -22,5 +27,8 @@ res.send(responseText)
 })
 app.use('/',PayMentRouter)
 app.use('/', EppRouter)
-
+app.use('contact/',ContactRouter)
+app.use('domain/',DomainRouter)
+app.use('host/',HostRouter)
+app.use('transaction/',TransactionRouter)
 app.listen(config.get("server.port"))
